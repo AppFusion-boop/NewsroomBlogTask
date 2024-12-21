1b325c7b345e9c367b4e74c0aa50e7cc2918404d
AppFusion-boop <appfusion2024@gmail.com>
2024-12-21 13:07:38 +0400

    update static files settings

--- PalitraBlog/wsgi.py
+++ PalitraBlog/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PalitraBlog.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+# application.add_files(settings.MEDIA_ROOT, prefix='media/')
+application.add_files(settings.STATIC_ROOT, prefix='static/')
